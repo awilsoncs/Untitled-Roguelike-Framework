7c488d43557319c2b195f2dc85f7b82424f93b64
Aaron Wilson <acwilsoncs@gmail.com>
2022-10-13 20:18:46 -0600

    straighten out the factory and entity stuff

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -3,21 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardController : PersistableObject, IBoardController
+public class BoardController : PersistableObject
 {
     const int saveVersion = 0;
 
-    private List<IEntity> entities;
-    private Dictionary<int, GameObject> pawn_reference;
-    private List<GameObject> pawns;
-    private const float GRID_MULTIPLE = 0.5f;
+    List<Entity> entities;
+    Dictionary<int, GameObject> entities_by_id;
+    const float GRID_MULTIPLE = 0.5f;
 
-    [SerializeField]
-    private GameObject mainCharacterPawn;
+    [SerializeField] GameObject mainCharacterPawn;
+    [SerializeField] PersistentStorage storage;
+    [SerializeField] EntityFactory entityFactory;
 
-    [SerializeField]
-    PersistentStorage storage;
+    public static BoardController Instance { get; set; }
 
+    void OnEnable () {
+        Instance = this;
+	}
+    
     private void Start() {
         BeginGame();
     }
@@ -29,18 +32,15 @@ public class BoardController : PersistableObject, IBoardController
     }
 
     private void BeginGame() {
-        entities = new List<IEntity>();
-        pawns = new List<GameObject>();
-        pawn_reference = new Dictionary<int, GameObject>();
+        entities = new List<Entity>();
+        entities_by_id = new Dictionary<int, GameObject>();
         CreateMainCharacter();
     }
 
     private void CreateMainCharacter() {
-        var mob = new Entity(this, 0);
-        var pawn = Instantiate(mainCharacterPawn);
-        entities.Add(mob);
-        pawn_reference.Add(0, pawn);
-        pawns.Add(pawn);
+        var entity = entityFactory.Get(0);
+        entities.Add(entity);
+        entities_by_id.Add(0, entity.gameObject);
         SetPawnPosition(0, 0, 0);
     }
 
@@ -50,11 +50,12 @@ public class BoardController : PersistableObject, IBoardController
 
     public void SetPawnPosition(int id, int x, int y) {
         Debug.Log($"Moving pawn {id} to ({x},{y})");
-        Transform pawn_transform = pawn_reference[id].transform;
-        pawn_transform.position = new Vector3(x*GRID_MULTIPLE, y*GRID_MULTIPLE, 0f);
+        Transform entity_transform = entities_by_id[id].transform;
+        entity_transform.position = new Vector3(x*GRID_MULTIPLE, y*GRID_MULTIPLE, 0f);
     }
 
     public String GetUserInputAction() {
+        // todo figure out why this moves twice
         if (Input.GetKeyDown(KeyCode.LeftArrow)) return "left";
         if (Input.GetKeyDown(KeyCode.RightArrow)) return "right";
         if (Input.GetKeyDown(KeyCode.UpArrow)) return "up";
@@ -79,11 +80,11 @@ public class BoardController : PersistableObject, IBoardController
     }
 
     public void ClearGame() {
-        foreach (var pawn in pawns) {
-            Destroy(pawn);            
+        foreach (var entity in entities) {
+            Destroy(entity.gameObject);            
         }
-        pawns.Clear();
-        pawn_reference.Clear();
+        entities.Clear();
+        entities_by_id.Clear();
     }
 
     public override void Save(GameDataWriter writer) {
