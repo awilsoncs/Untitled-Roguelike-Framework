85546de10fdc8914fc53df303c01b1916c20bb2a
Aaron Wilson <acwilsoncs@gmail.com>
2022-10-23 10:21:38 -0600

    Finish basic FOV implementation

--- Assets/Scripts/Algorithms.cs
+++ Assets/Scripts/Algorithms.cs
@@ -15,6 +15,31 @@ public static class Algorithms {
         public delegate bool PlotFunction(int x, int y);
 
         /// <summary>
+        /// Runs Bresenham's line algorithm, reverse corrected.
+        /// </summary>
+        public static void Line(int x0, int y0, int x1, int y1, PlotFunction plot) {
+            // Explanation: in cases where the start is to the right of the end,
+            // Bresenham's algorithm returns the line from end to start.
+            // todo can we just fix the bresenham algorithm to not reverse?
+            var line = new List<(int, int)>();
+            Bresenham(
+                x0, y0, x1, y1,
+                (x, y) => {
+                    line.Add((x, y));
+                    return true;
+                }
+            );
+            if (!(x0, y0).Equals(line[0])) {
+                line.Reverse();
+            }
+
+            for(int i = 0; i < line.Count; i++) {
+                bool result = plot(line[i].Item1, line[i].Item2);
+                if (!result) {break;}
+            }
+        }
+
+        /// <summary>
         /// Plot the line from (x0, y0) to (x1, y1)
         /// </summary>
         /// <param name="x0">The start x</param>
@@ -22,7 +47,7 @@ public static class Algorithms {
         /// <param name="x1">The end x</param>
         /// <param name="y1">The end y</param>
         /// <param name="plot">The plotting function (if this returns false, the algorithm stops early)</param>
-        public static void Line(int x0, int y0, int x1, int y1, PlotFunction plot)
+        public static void Bresenham(int x0, int y0, int x1, int y1, PlotFunction plot)
         {
             bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
             if (steep) { Swap<int>(ref x0, ref y0); Swap<int>(ref x1, ref y1); }
--- Assets/Scripts/FieldOfView/RaycastingFOV.cs
+++ Assets/Scripts/FieldOfView/RaycastingFOV.cs
@@ -5,24 +5,22 @@ using System.Collections.Generic;
 /// Performs FOV calculation by raycasting to edge tiles.
 /// </summary>
 public class RaycastingFOV : IFieldOfView {
+    // todo this is quite slow and should eventually be replaced with a shadowcasting
     public IFieldOfViewQueryResult CalculateFOV(IGameState gameState, int x0, int y0) {
         var results = new Dictionary<(int, int), bool>();
         Algorithms.PlotFunction pf = (int x, int y) => {
-            if (gameState.GetCell(x, y).IsTransparent) {
-                results[(x, y)] = true;
-                return true;
-            }
-            return false;
+            results[(x, y)] = true;
+            return gameState.GetCell(x, y).IsTransparent;
         };
 
         // for each point on the boundary top and bottom
-        for (int x1 = 0; x1 < gameState.MapWidth; x1++) {
-            Algorithms.Line(x1, 0, x0, y0, pf);
-            Algorithms.Line(x1, gameState.MapHeight-1, x0, y0, pf);
+        for (int column = 0; column < gameState.MapWidth; column++) {
+            Algorithms.Line(x0, y0, column, gameState.MapHeight-1, pf);
+            Algorithms.Line(x0, y0, column, 0, pf);
         }
-        for (int y1 = 0; y1 < gameState.MapHeight; y1++) {
-            Algorithms.Line(0, y1, x0, y0, pf);
-            Algorithms.Line(gameState.MapWidth - 1, y1, x0, y0, pf);
+        for (int row = 0; row < gameState.MapHeight; row++) {
+            Algorithms.Line(x0, y0, 0, row, pf);
+            Algorithms.Line(x0, y0, gameState.MapWidth - 1, row, pf);
         }
             // stop when you find something that blocks sight
             // update the query result
