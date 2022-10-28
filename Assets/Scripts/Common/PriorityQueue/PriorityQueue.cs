e6834bdb9edecb7e5e851aaf49344a4f290b3f47
Aaron Wilson <acwilsoncs@gmail.com>
2022-10-28 11:49:46 -0600

    add new queries to PriorityQueue

--- Assets/Scripts/Common/PriorityQueue/PriorityQueue.cs
+++ Assets/Scripts/Common/PriorityQueue/PriorityQueue.cs
@@ -4,20 +4,34 @@ public class PriorityQueue<T, U> : IPriorityQueue<T, U>
 {
     public int Count => elements.Count;
     private SortedList<T, U> elements;
+    private Dictionary<U, T> prioritiesByElement;
 
     public PriorityQueue() {
         elements = new SortedList<T, U>();
+        prioritiesByElement = new Dictionary<U, T>();
     }
 
     public U Dequeue()
     {
         U result = elements.Values[0];
         elements.RemoveAt(0);
+        prioritiesByElement.Remove(result);
         return result;
     }
+    // todo support reverse priority
 
     public void Enqueue(T priority, U element)
     {
         elements.Add(priority, element);
+        prioritiesByElement.Add(element, priority);
+    }
+
+    public T PriorityOf(U element) {
+        // todo element may not be in dict
+        return prioritiesByElement[element];
+    }
+
+    public bool Contains(U element) {
+        return prioritiesByElement.ContainsKey(element);
     }
 }
--- Assets/Tests/PriorityQueueTests.cs
+++ Assets/Tests/PriorityQueueTests.cs
@@ -17,20 +17,20 @@ public class PriorityQueueTests
     [Test]
     public void CountIncreasesAfterEnqueue()
     {
-        Assert.That(queue.Count, Is.EqualTo(0));
+        Assert.That(queue.Count, Is.EqualTo(0), "Queue count should begin at 0.");
         queue.Enqueue(2, 2);
-        Assert.That(queue.Count, Is.EqualTo(1));
+        Assert.That(queue.Count, Is.EqualTo(1), "Queue count should increment after enqueuing.");
     }
 
     [Test]
     public void CountDecreasesAfterDequeue() {
         queue.Enqueue(1, 1);
         queue.Enqueue(2, 2);
-        Assert.That(queue.Count, Is.EqualTo(2));
+        Assert.That(queue.Count, Is.EqualTo(2), "Queue count should be at 2 after enqueuing twice.");
         queue.Dequeue();
-        Assert.That(queue.Count, Is.EqualTo(1));
+        Assert.That(queue.Count, Is.EqualTo(1), "Queue should decrement after dequeuing");
         queue.Dequeue();
-        Assert.That(queue.Count, Is.EqualTo(0));
+        Assert.That(queue.Count, Is.EqualTo(0), "Queue should decrement twice after dequeuing twice.");
     }
 
     [Test]
@@ -41,10 +41,34 @@ public class PriorityQueueTests
         stringQueue.Enqueue(2, "world");
         stringQueue.Enqueue(0, "hello");
         var firstString = stringQueue.Dequeue();
-        Assert.That(firstString, Is.EqualTo("hello"));
+        Assert.That(firstString, Is.EqualTo("hello"), "Queue should place lowest priority score item at front.");
         var secondString = stringQueue.Dequeue();
-        Assert.That(secondString, Is.EqualTo("there"));
+        Assert.That(secondString, Is.EqualTo("there"), "Queue should place middle priority score item in the middle.");
         var thirdString = stringQueue.Dequeue();
-        Assert.That(thirdString, Is.EqualTo("world"));
+        Assert.That(thirdString, Is.EqualTo("world"), "Queue should place highest priority score item at the back.");
+    }
+
+    [Test]
+    public void QueueCanBeQueriedForElementPriority() {
+        queue.Enqueue(1, 1);
+        queue.Enqueue(2, 3);
+        queue.Enqueue(5, 6);
+
+        Assert.That(queue.PriorityOf(1), Is.EqualTo(1), "Queue should return correct priority when queried.");
+        Assert.That(queue.PriorityOf(3), Is.EqualTo(2), "Queue should return correct priority when queried.");
+        Assert.That(queue.PriorityOf(6), Is.EqualTo(5), "Queue should return correct priority when queried.");
+    }
+
+    [Test]
+    public void QueueReportsItContainsContainedElement() {
+        queue.Enqueue(2, 3);
+        Assert.IsTrue(queue.Contains(3), "Queue should report that it contains an item it contains.");
+    }
+
+    
+    [Test]
+    public void QueueReportsItDoesNotContainMissingElement() {
+        queue.Enqueue(2, 3);
+        Assert.IsFalse(queue.Contains(5), "Queue should report that it does not contain an item it doesn't contain.");
     }
 }
