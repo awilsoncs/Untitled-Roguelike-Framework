123c331c976cf9ec8d126e14746f10770d5d7589
Aaron Wilson <acwilsoncs@gmail.com>
2022-11-05 12:14:36 -0600

    add doc comment to GameStartedEvent

--- Assets/Scripts/Common/GameEvents/GameStartedEvent.cs
+++ Assets/Scripts/Common/GameEvents/GameStartedEvent.cs
@@ -1,3 +1,6 @@
+/// <summary>
+/// Notify listeners that a new game has begun.
+/// </summary>
 public struct GameStartedEvent : IGameEvent {
     public GameEventType EventType => GameEventType.GameStarted;
 }
