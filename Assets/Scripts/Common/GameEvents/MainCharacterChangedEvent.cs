4ce4632a800fa92ab71b3ec0c46bf0b81ed2b056
Aaron Wilson <acwilsoncs@gmail.com>
2022-11-05 12:15:41 -0600

    add comments and todos to MainCharacterChanged

--- Assets/Scripts/Common/GameEvents/MainCharacterChangedEvent.cs
+++ Assets/Scripts/Common/GameEvents/MainCharacterChangedEvent.cs
@@ -1,5 +1,12 @@
+/// <summary>
+/// Notify listeners that the main character has changed.
+/// </summary>
 public struct MainCharacterChangedEvent : IGameEvent {
+    /// <summary>
+    /// The new main character
+    /// </summary>
     public IEntity Entity {get;}
+    // todo add a reference to the old main character here
     public GameEventType EventType => GameEventType.MainCharacterChanged;
 
     public MainCharacterChangedEvent (IEntity entity) {
