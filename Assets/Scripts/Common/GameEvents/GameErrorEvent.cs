a97c9c14500469c14ca48ec5cd618d8cdb6bf7df
Aaron Wilson <acwilsoncs@gmail.com>
2022-11-05 12:13:25 -0600

    clean up game error events

--- Assets/Scripts/Common/GameEvents/GameErrorEvent.cs
+++ Assets/Scripts/Common/GameEvents/GameErrorEvent.cs
@@ -1,6 +1,12 @@
 using System;
 
+/// <summary>
+/// Notify listeners that the game has experienced an error.
+/// </summary>
 public struct GameErrorEvent : IGameEvent {
+    /// <summary>
+    /// The error message
+    /// </summary>
     public string Message { get; set; }
 
     public GameEventType EventType => GameEventType.GameError;
