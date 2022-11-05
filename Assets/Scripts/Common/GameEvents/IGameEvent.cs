53dd0bf8625d592c4281a0c8f3054308fbc0b0b6
Aaron Wilson <acwilsoncs@gmail.com>
2022-11-05 11:50:11 -0600

    move events to common and change attacked event ids to entity

---
+++ Assets/Scripts/Common/GameEvents/IGameEvent.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// An event from the game state to the client.
+/// </summary>
+public interface IGameEvent {
+    /// <summary>
+    /// Provide the event type for parsing on the client side.
+    /// </summary>
+    /// <value>The corresponding event type for this event.</value>
+    GameEventType EventType {get;}
+}
