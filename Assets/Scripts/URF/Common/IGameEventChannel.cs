06bcc2aefc294a97f5518df8ac1b6adfbe6aceae
Aaron Wilson <acwilsoncs@gmail.com>
2022-11-17 09:18:23 -0700

    Add a comment to IGameEventChannel
    Small commit to trigger a push -> test action on github

--- Assets/Scripts/URF/Common/IGameEventChannel.cs
+++ Assets/Scripts/URF/Common/IGameEventChannel.cs
@@ -4,6 +4,9 @@ using URF.Common.GameEvents;
 namespace URF.Common {
   public interface IGameEventChannel {
 
+    /// <summary>
+    /// Message containing declarative game events.
+    /// </summary>
     public event EventHandler<IGameEventArgs> GameEvent;
 
   }
