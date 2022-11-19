5497a41d53a3ba35adc491b42c6af7b779c4c34d
Aaron Wilson <acwilsoncs@gmail.com>
2022-11-19 13:51:33 -0700

    Extract test numbers into well-named variables

--- Assets/Tests/Server/FieldOfViewQueryResultTests.cs
+++ Assets/Tests/Server/FieldOfViewQueryResultTests.cs
@@ -5,49 +5,53 @@ namespace Tests.Server {
   using URF.Server.FieldOfView;
 
   public class FieldOfViewQueryResultTests {
-    private readonly Position cell = (5, 8);
+    private readonly Position testCell = (5, 8);
 
     [Test]
     public void FieldOfViewQueryResult_Should_ShowVisiblePositionIsVisible() {
       var results = new Dictionary<Position, bool> {
-        [this.cell] = true
+        [this.testCell] = true
       };
 
       var query = new FieldOfViewQueryResult(results);
-      Assert.That(query.IsVisible((5, 8)), "Visible position wasn't returned visible.");
+      Assert.That(query.IsVisible(this.testCell), "Visible position wasn't returned visible.");
     }
 
     [Test]
     public void FieldOfViewQueryResult_Should_ShowNotVisiblePositionIsNotVisible() {
       var results = new Dictionary<Position, bool> {
-        [(5, 8)] = false
+        [this.testCell] = false
       };
 
       var query = new FieldOfViewQueryResult(results);
-      Assert.That(!query.IsVisible((5, 8)), "Not visible position was returned visible.");
+      Assert.That(!query.IsVisible(this.testCell), "Not visible position was returned visible.");
     }
 
     [Test]
     public void FieldOfViewQueryResult_Should_DefaultToNotVisible() {
       var results = new Dictionary<Position, bool> {
-        [(5, 8)] = false
+        [this.testCell] = false
       };
 
       var query = new FieldOfViewQueryResult(results);
-      Assert.That(!query.IsVisible((8, 13)), "Query result should default to not visible.");
+      var otherPosition = new Position(8, 13);
+      Assert.That(!query.IsVisible(otherPosition), "Query result should default to not visible.");
     }
 
     [Test]
     public void FieldOfViewQueryResult_Should_OnlyShowVisibleIfThisExactCellIsVisible() {
       // test that results don't return true if column/row are right but both aren't
+      var otherVisibleCell = new Position(6, 9);
       var results = new Dictionary<Position, bool> {
-        [(5, 8)] = true,
-        [(6, 9)] = true
+        [this.testCell] = true,
+        [otherVisibleCell] = true
       };
+
+      var notVisibleCell = new Position(5, 9);
 
       var query = new FieldOfViewQueryResult(results);
       Assert.That(
-        !query.IsVisible((5, 9)),
+        !query.IsVisible(notVisibleCell),
         "Query result should require row/column pair to be visible."
       );
     }
