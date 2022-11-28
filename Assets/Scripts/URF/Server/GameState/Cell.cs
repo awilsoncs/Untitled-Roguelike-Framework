3abc7b69219e4cb5da232a9d35980fb06029b432
Aaron Wilson <acwilsoncs@gmail.com>
2022-11-28 11:09:57 -0700

    migrate Cell.cs to new style

--- Assets/Scripts/URF/Server/GameState/Cell.cs
+++ Assets/Scripts/URF/Server/GameState/Cell.cs
@@ -1,39 +1,38 @@
-using System.Collections.Generic;
-using System.Linq;
-using URF.Common.Entities;
-using URF.Server.RulesSystems;
-
 namespace URF.Server.GameState {
+  using System.Collections.Generic;
+  using System.Linq;
+  using URF.Common.Entities;
+  using URF.Server.RulesSystems;
+
   /// <summary>
   /// Cells provide a geometric container for game objects. Cells do NOT provide
   /// any object management functionality.
   /// </summary>
   public class Cell {
 
-    public HashSet<IEntity> Contents { get; }
+    public HashSet<IEntity> Contents {
+      get;
+    }
+
+    public bool IsPassable => this.Contents.Count == 0 ||
+          this.Contents.Select(x => x.GetComponent<Movement>()).All(x => !x.BlocksMove);
+
+
+    public bool IsTransparent => this.Contents.Count == 0 || !this.Contents.Any(x => x.BlocksSight);
+
 
     public Cell() {
-      Contents = new HashSet<IEntity>();
+      this.Contents = new HashSet<IEntity>();
     }
 
     public void PutContents(IEntity entity) {
-      Contents.Add(entity);
+      _ = this.Contents.Add(entity);
     }
 
     public void RemoveEntity(IEntity entity) {
-      if(Contents.Contains(entity)) { Contents.Remove(entity); }
-    }
-
-    public bool IsPassable {
-      get {
-        return Contents.Count == 0 ||
-          Contents.Select(x => x.GetComponent<Movement>()).All(x => !x.BlocksMove);
+      if (this.Contents.Contains(entity)) {
+        _ = this.Contents.Remove(entity);
       }
     }
-
-    public bool IsTransparent {
-      get { return Contents.Count == 0 || !Contents.Any(x => x.BlocksSight); }
-    }
-
   }
 }
