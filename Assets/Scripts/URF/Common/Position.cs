3628c707e704460da0e109b910b406bedbb12a92
Aaron Wilson <acwilsoncs@gmail.com>
2023-01-17 20:55:29 -0700

    more codacy touchup

--- Assets/Scripts/URF/Common/Position.cs
+++ Assets/Scripts/URF/Common/Position.cs
@@ -1,47 +1,66 @@
 namespace URF.Common {
+  /// <summary>
+  /// Represent a point in the 2D game world.
+  /// </summary>
   public readonly struct Position {
 
-    public int X { get; }
+    /// <summary>
+    /// The horizontal coordinate
+    /// </summary>
+    public int X {
+      get;
+    }
 
-    public int Y { get; }
+
+    /// <summary>
+    /// The vertical coordinate
+    /// </summary>
+    public int Y {
+      get;
+    }
 
     public Position(int x, int y) {
-      X = x;
-      Y = y;
+      this.X = x;
+      this.Y = y;
     }
 
+    /// <summary>
+    /// Return whether this position is possible in a 2D natural number matrix
+    /// </summary>
     public bool IsValidPosition => this.X >= 0 && this.Y >= 0;
 
-    public static implicit operator Position((int, int) t) {
-      return new Position(t.Item1, t.Item2);
-    }
+    public static implicit operator Position((int, int) t) => new(t.Item1, t.Item2);
 
     public void Deconstruct(out int x, out int y) {
-      x = X;
-      y = Y;
+      x = this.X;
+      y = this.Y;
     }
 
     public override string ToString() {
-      return $"({X},{Y})";
+      return $"({this.X},{this.Y})";
     }
 
-    public static Position operator +(Position first, Position second) {
-      return (first.X + second.X, first.Y + second.Y);
+    /// <summary>
+    /// Add two positions using vector addition
+    /// </summary>
+    public static Position operator +(Position first, Position second)
+      => (first.X + second.X, first.Y + second.Y);
+
+    /// <summary>
+    /// Provide the invalid position
+    /// </summary>
+    public static Position Invalid = new(-1, -1);
+
+    public override bool Equals(object obj) {
+      return (obj is Position position) && this.Equals(position);
     }
 
-    public static Position Invalid => new(-1, -1);
-    
-    public override bool Equals(object other) {
-      return (other is Position position) && this.Equals(position);
-    }
-    
     public bool Equals(Position other) {
       return this.X == other.X && this.Y == other.Y;
     }
-    
-    public override int GetHashCode() {
-      return (this.X, this.Y).GetHashCode();
-    }
 
+    public override int GetHashCode() {
+      return System.HashCode.Combine(this.X, this.Y);
+    }
   }
 }
